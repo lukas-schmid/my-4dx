0af1d1eca55cafeaac0879b51a9506d10e4e542e
lukas-schmid <schmid.luk@hotmail.com>
2021-03-26 15:29:56 +0100

    setup update

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");
 require("dotenv").config();
 
-const serviceAccount = require({
+const serviceAccount = {
   type: process.env.TYPE,
   project_id: process.env.PROJECT_ID,
   private_key_id: process.env.PRIVATE_KEY_ID,
@@ -12,7 +12,7 @@ const serviceAccount = require({
   token_uri: process.env.TOKEN_URI,
   auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
   client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
-});
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
