a6681fd337c8b9cdccf104872be5f87cd1fe4388
petter0619 <petter0619@gmail.com>
2021-03-30 11:46:57 +0200

    Added support for a non-random quote

--- client/src/components/PageFooter.js
+++ client/src/components/PageFooter.js
@@ -4,17 +4,17 @@ import quotesList from '../assets/quotes.json';
 import { randomItemFromArray } from '../helpers';
 import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
 
-export default function PageFooter() {
+export default function PageFooter({nonRandomQuote}) {
 
-    const { quote, by } = randomItemFromArray(quotesList);
+    const quote = nonRandomQuote || randomItemFromArray(quotesList);
 
     return (
         <div className="page-footer">
             <div className="quote">
                 <FaQuoteLeft className="quote__svg quote__svg--left"/>
-                <p className="quote__text">{quote}</p>
+                <p className="quote__text">{quote.quote}</p>
                 <FaQuoteRight className="quote__svg quote__svg--right"/>
-                {by && <span className="quote__by">- {by}</span>}
+                {quote.by && <span className="quote__by">- {quote.by}</span>}
             </div>
         </div>
     )
