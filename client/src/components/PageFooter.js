9212d100ffeeb47fcd4f648321af061448ba6926
petter0619 <petter0619@gmail.com>
2021-03-30 10:23:40 +0200

    Login form styling

--- client/src/components/PageFooter.js
+++ client/src/components/PageFooter.js
@@ -1,23 +1,19 @@
 import React from 'react';
 
 import quotesList from '../assets/quotes.json';
+import { randomItemFromArray } from '../helpers';
 import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
 
-export default function QuoteFooter() {
-    function randomItemFromArray(arr, not) {
-        const item = arr[Math.floor(Math.random() * arr.length)];
-        if(item === not) return randomItemFromArray(arr, not);
-        return item;
-    }
+export default function PageFooter() {
 
     const { quote, by } = randomItemFromArray(quotesList);
 
     return (
         <div className="page-footer">
             <div className="quote">
-                <FaQuoteLeft className="quote__svg--left"/>
+                <FaQuoteLeft className="quote__svg quote__svg--left"/>
                 <p className="quote__text">{quote}</p>
-                <FaQuoteRight className="quote__svg--right"/>
+                <FaQuoteRight className="quote__svg quote__svg--right"/>
                 {by && <span className="quote__by">- {by}</span>}
             </div>
         </div>
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,18 +1,24 @@
 import React from 'react';
 // Components
-import QuoteFooter from '../components/QuoteFooter';
+import PageFooter from '../components/PageFooter';
 import PageHeader from '../components/PageHeader';
 import LoginForm from '../components/LoginForm';
 
 export default function Login() {
     return (
         <main className="page-container">
-            <section className="page-content">
+            <section className="page-content page-split login-page">
                 <PageHeader pageTitle="My 4DX"/>
                 <article className="form-container">
                     <LoginForm />
+                    <hr />
+                    <div className="demo-login">
+                        <p className="form-text demo-login__text">Try a demo user!</p>
+                        <button className="btn btn-primary demo-login__btn">Team Admin</button>
+                        <button className="btn btn-primary demo-login__btn">Team Member</button>
+                    </div>
                 </article>
-                <QuoteFooter />
+                <PageFooter />
             </section>
         </main>
     )
