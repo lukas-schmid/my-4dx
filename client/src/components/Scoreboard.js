94e06b1d69332be055f74710e119a8298df58a91
johanlyckenvik <johan.lyckenvik@gmail.com>
2021-03-31 15:07:34 +0200

    updated scoreboard

--- client/src/components/Scoreboard.js
+++ client/src/components/Scoreboard.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Bar } from '@reactchartjs/react-chart.js'
+import { Line } from '@reactchartjs/react-chart.js'
 
-const data = {
+const dataBar = {
   labels: ['Person 1', 'Person 2', 'Person 3', 'Person 4', 'Person 5', 'Person 6'],
   datasets: [
     {
-      data: [12, 19, 3, 5, 2, 3],
+        label: '# of cold calls',
+        data: [12, 19, 3, 5, 2, 3],
       backgroundColor: [
         'rgba(21, 115, 71, 0.4)',
         'rgba(0, 108, 87, 0.4)',
@@ -27,7 +29,27 @@ const data = {
   ],
 }
 
-const options = {
+const dataLine = {
+    labels: ['1', '2', '3', '4', '5', '6'],
+    datasets: [
+      {
+        label: '# of cold calls / team',
+        data: [12, 19, 3, 5, 2, 3],
+        fill: false,
+        backgroundColor: 'rgb(47, 72, 88)',
+        borderColor: 'rgba(47, 72, 88, 0.2)',
+      },
+      {
+        label: 'baseline',
+        data: [6, 6, 6, 8, 8, 8],
+        fill: false,
+        backgroundColor: 'rgb(21, 115, 71)',
+        borderColor: 'rgba(21, 115, 71, 0.2)',
+      },
+    ],
+  }
+
+const optionsBar = {
   scales: {
     yAxes: [
       {
@@ -39,12 +61,35 @@ const options = {
   },
 }
 
+const optionsLine = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  }
+
 const Scoreboard = () => (
   <>
-    <div className='header'>
-      <h1 className='title'>Scoreboard title</h1>
+    <div className= 'scoreBoard'>
+    <div style={{
+            backgroundColor: 'white',
+            width: 500,
+            }}>
+        <h2 style={{textAlign: 'center'}}>Individual performances week 6</h2>
+        <Bar data={dataBar} options={optionsBar} />
     </div>
-    <Bar data={data} options={options} />
+    <div style={{
+            backgroundColor: 'white',
+            width: 500,
+            }}>
+        <h2 style={{textAlign: 'center'}}>Team performance past 6 weeks</h2>
+        <Line data={dataLine} options={optionsLine} />    </div>
+    </div>
   </>
 )
 
