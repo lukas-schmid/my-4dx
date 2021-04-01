b0b63aec8bee3356b677d887c99e171214f5f4e5
petter0619 <petter0619@gmail.com>
2021-04-01 17:04:12 +0200

    Restyled My Account page

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -7,7 +7,7 @@ import UserAccountForm from '../components/UserAccountForm';
 export default function Account() {
     return (
         <main className="page-container">
-            <section className="page-content my-account-page">
+            <section className="page-content page-split my-account-page">
                 <PageHeader pageTitle="My Account"/>
                 <article className="form-container">
                     <UserAccountForm />
