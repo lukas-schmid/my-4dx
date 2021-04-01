6853a0bbdae2cb1944bf1cb2e232179c6db5e995
lukas-schmid <schmid.luk@hotmail.com>
2021-04-01 13:08:45 +0200

    fetchFunctions clean up

--- client/src/fetchFunctions.js
+++ client/src/fetchFunctions.js
@@ -1,29 +1,9 @@
-// export function postFetch(url, body) {
-//   const requestOptions = {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify(body),
-//   };
-//   fetch(url, requestOptions)
-//     .then(async (response) => {
-//       const data = await response.json();
-//       if (!response.ok) {
-//         const error = (data && data.message) || response.status;
-//         return Promise.reject(error);
-//       }
-//       return data;
-//     })
-//     .catch((error) => {
-//       return error;
-//     });
-// }
-
 export async function postFetch(url, reqBody) {
   const requestOptions = {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(reqBody),
-  }
+  };
   try {
     const json = await fetch(url, requestOptions);
     const data = await json.json();
@@ -33,54 +13,39 @@ export async function postFetch(url, reqBody) {
   }
 }
 
-export function getFetch(url) {
-  fetch(url)
-    .then(async (response) => {
-      const data = await response.json();
-      if (!response.ok) {
-        const error = (data && data.message) || response.status;
-        return Promise.reject(error);
-      }
-      return data;
-    })
-    .catch((error) => {
-      return error;
-    });
+export async function getFetch(url) {
+  try {
+    const json = await fetch(url);
+    const data = await json.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 }
 
-export function putFetch(url, body) {
+export async function putFetch(url, reqBody) {
   const requestOptions = {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(body),
+    body: JSON.stringify(reqBody),
   };
-  fetch(url, requestOptions)
-    .then(async (response) => {
-      const data = await response.json();
-      if (!response.ok) {
-        const error = (data && data.message) || response.status;
-        return Promise.reject(error);
-      }
-      return data;
-    })
-    .catch((error) => {
-      return error;
-    });
+  try {
+    const json = await fetch(url, requestOptions);
+    const data = await json.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 }
 
-export function deleteFetch(url) {
-  fetch(url, {
-    method: "DELETE",
-  })
-    .then(async (response) => {
-      const data = await response.json();
-      if (!response.ok) {
-        const error = (data && data.message) || response.status;
-        return Promise.reject(error);
-      }
-      return data;
-    })
-    .catch((error) => {
-      return error;
+export async function deleteFetch(url) {
+  try {
+    const json = await fetch(url, {
+      method: "DELETE",
     });
+    const data = await json.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 }
