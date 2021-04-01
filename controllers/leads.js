41d23c2899b7e194666f1bada62e183accef588a
lukas-schmid <schmid.luk@hotmail.com>
2021-04-01 12:50:40 +0200

    update leads done

--- controllers/leads.js
+++ controllers/leads.js
@@ -29,29 +29,33 @@ exports.createLead = async (req, res, next) => {
 };
 
 exports.updateLead = async (req, res, next) => {
+  const wigId = req.params.wigId;
   const leadId = req.params.leadId;
-  const leadName = req.body.leadName;
-  const leadInterval = req.body.leadInterval;
-  const leadDataType = req.body.leadDataType;
-  const benchmarkExists = req.body.benchmarkExists;
-  const benchmark = req.body.benchmark;
-  const leadData = [
+  const leadMeasures = [
     {
-      startDate: req.body.leadData[0].startDate,
-      date: req.body.leadData[0].data,
+      leadId: req.params.leadId,
+      leadName: req.body.leadName,
+      leadInterval: req.body.leadInterval,
+      leadDataType: req.body.leadDataType,
+      benchmarkExists: req.body.benchmarkExists,
+      benchmark: req.body.benchmark,
+      leadData: [
+        {
+          startDate: req.body.leadData[0].startDate,
+          date: req.body.leadData[0].data,
+        },
+      ],
     },
   ];
   try {
-    await leadService.updateLead(
-      leadId,
-      leadName,
-      leadInterval,
-      leadDataType,
-      benchmarkExists,
-      benchmark,
-      leadData
+    const wig = await wigService.getWig(wigId);
+    const currentLeadMeasures = wig.leadMeasures;
+    const filteredLeadMeasures = currentLeadMeasures.filter(
+      (obj) => obj.leadId !== leadId
     );
-    const response = await leadService.getLead(leadId);
+    const newLeadMeasures = filteredLeadMeasures.concat(leadMeasures);
+    await leadService.addLeadToWig(wigId, newLeadMeasures);
+    const response = await wigService.getWig(wigId);
     res.status(200).json(response);
   } catch (error) {
     next(error);
--- routes/leads.js
+++ routes/leads.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const leads = require("../controllers/leads");
 
+router.put("/api/:wigId/leads/:leadId", leads.updateLead);
 router.post("/api/:wigId/leads", leads.createLead);
-router.put("/api/leads/:leadId", leads.updateLead);
 router.delete("/api/leads/:leadId", leads.deleteLead);
 
 module.exports = router;
