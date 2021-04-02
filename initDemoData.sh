c7be4a00ed35eb8fc4653c64564aadf0d702c5e0
lukas-schmid <schmid.luk@hotmail.com>
2021-04-02 21:41:57 +0200

    commented out lead update in.sh bc must be changed

--- initDemoData.sh
+++ initDemoData.sh
@@ -85,23 +85,31 @@ echo "request body: $body"
 leadId=$(curl -w "\n" -d "$body" -H 'Content-Type: application/json' http://localhost:8080/api/"$wigId"/leads | jq -r .leadMeasures[0].leadId)
 
 # 4.2 put("/api/:wigId/leads/:leadId")
-body=$(jq -n --arg b "$teamId" '{
-    "leadName": "999 phone calls per day",
-    "leadInterval": "daily",
-    "leadDataType": "money",
-    "benchmarkExists": false,
-    "benchmark": "555",
-    "leadData": [
-        {
-            "startDate": "2020-07-01",
-            "data": 20
-        }
-    ]
-}')
+# body=$(jq -n --arg b "$teamId" '{
+#     "leadName": "999 phone calls per day",
+#     "leadInterval": "daily",
+#     "leadDataType": "money",
+#     "benchmarkExists": false,
+#     "benchmark": "555",
+#     "leadData": [
+#         {
+#             "startDate": "2020-07-01",
+#             "data": 20
+#         }
+#     ]
+# }')
 
-echo "PUT http://localhost:8080/api/$wigId/leads/$leadId"
-echo "request body: $body"
-curl -w "\n" -d "$body" -H 'Content-Type: application/json' -X PUT http://localhost:8080/api/"$wigId"/leads/"$leadId" | jq .
+# echo "PUT http://localhost:8080/api/$wigId/leads/$leadId"
+# echo "request body: $body"
+# curl -w "\n" -d "$body" -H 'Content-Type: application/json' -X PUT http://localhost:8080/api/"$wigId"/leads/"$leadId" | jq .
+
+
+
+
+
+
+
+
 
 # 5. Lag
 # 5.1 put("/api/lag/:wigId")
