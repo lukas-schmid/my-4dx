a623ef462368bcfadddd91ab7995a0e73dbc540f
johanlyckenvik <johan.lyckenvik@gmail.com>
2021-04-02 16:00:52 +0200

    day5

---
+++ client/src/mockData.js
@@ -0,0 +1,22 @@
+export const dataSet1 = {
+    "lagBaseline": [10, 20, 20, 20, 25, 40],
+    "lagData": [12, 15 ,18, 21, 22, 35],
+    "lagType": "Sales",
+    "leadBaseline": [100, 150, 150, 150, 150, 150],
+    "leadData": [88, 137, 152, 141, 158, 152],
+    "recentLead": [32, 36, 44, 30, 7, 3],
+    "leadType": "Cold calls",
+    "dates": ["2021-01-11", "2021-01-18", "2021-01-25", "2021-02-01", "2021-02-08", "2021-02-15"],
+    "teamMembers": ["Simon", "Sara", "Guian", "Richard", "Bea", "Jenny"]
+}
+
+export const dataSet2 = {
+    "lagBaseline": [10, 20, 20, 20, 25, 40, 40, 40],
+    "lagData": [12, 15 ,18, 21, 22, 35, 23, 38],
+    "lagType": "Sales",
+    "leadBaseline": [100, 150, 150, 150, 150, 150, 150, 150],
+    "leadData": [88, 137, 152, 141, 158, 152, 89, 161],
+    "leadType": "Cold calls",
+    "dates": ["2021-01-11", "2021-01-18", "2021-01-25", "2021-02-01", "2021-02-08", "2021-02-15", "2021-02-22"],
+    "teamMembers": ["Simon", "Sara", "Guian", "Richard"]
+}
