8c735999ac12af5d523b2ca812adbc506dab8ded
lukas-schmid <schmid.luk@hotmail.com>
2021-04-03 23:05:18 +0200

    refactored and cleaned up

--- client/src/components/Scoreboard.js
+++ client/src/components/Scoreboard.js
@@ -4,12 +4,12 @@ import { Line } from '@reactchartjs/react-chart.js'
 import PageHeader from '../components/PageHeader';
 import { dataSet1 } from '../mockData';
 import { useGlobalContext } from '../appContext';
-import FormLoaderOverlay from './FormLoaderOverlay';
+// import FormLoaderOverlay from './FormLoaderOverlay';
 import { getAllWigsByTeamId, getTeamMembers } from '../apiHelper';
 import Select from 'react-select';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { registerLocale, setDefaultLocale } from  "react-datepicker";
+import { registerLocale } from  "react-datepicker";
 import en from 'date-fns/locale/en-GB';
 registerLocale('en', en)
 
@@ -19,117 +19,89 @@ export default function Scoreboard(){
   const { currentUserInfo ,isLoading, setIsLoading } = useGlobalContext();
   const [teamMembers, setTeamMembers] = useState([]);
   const [teamMembersDropdown, setTeamMembersDropdown] = useState();
-  const [wigs, setWigs] = useState([]);
-  const [leadMeasure, setLeadMeasure] = useState();
+  const [allWigs, setAllWigs] = useState([]);
+  const [currentWig, setCurrentWig] = useState();
+  const [currentLeadMeasure, setCurrentLeadMeasure] = useState();
   const [defaultLeadMeasureDropdownValue, setDefaultLeadMeasureDropdownValue] = useState();
-  const [selectedTeamMembers, setSeletedTeamMembers] = useState([]);
-  const [chartData, setChartData] = useState([]);
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(null);
   const [dateRange, setDateRange] = useState();
+  const [selectedTeamMembers, setSeletedTeamMembers] = useState([]);
 
   useEffect(() => {
-    setIsLoading(true);
-    getAllWigsByTeamId(currentUserInfo.teamId)
-      .then(data => {
-          setWigs(data);
-          let defaultWig = data[0];
-          setChartData([defaultWig]);
-          if (defaultWig !== undefined && [defaultWig][0].leadMeasures[0] !== undefined) {
-          setLeadMeasure([defaultWig][0].leadMeasures[0]);
-          const defaultDateRange = [defaultWig][0].leadMeasures[0].leadData.map(date => date.startDate);
-          setDateRange(defaultDateRange);
-          const startDate = new Date([defaultWig][0].startDate);
-          const endDate = new Date([defaultWig][0].endDate);
-          setStartDate(startDate);
-          setEndDate(endDate);
-
-          const dates = [defaultWig][0].leadMeasures[0].leadData.map(date => new Date(date.startDate).getTime());
-          const start = startDate.getTime();
-          const end = endDate.getTime();
-          const range = dates.filter(date => date >= start && date <= end)
-          const formattedDateArray = range.map(date => new Date(date).toISOString().split("T")[0]);
-          setDateRange(formattedDateArray);
-          //handleDateFilter(startDate, endDate);
-          }
-          setIsLoading(false);
-      })
-      .catch(err => {
-          // console.log(wigs);
-          // setChartData([])
-          // console.log(chartData)
-          setIsLoading(false);
-          console.error(err);
-      });
-
     getTeamMembers(currentUserInfo.teamId)
       .then(data => {
           setTeamMembers(data);
           setTeamMembersDropdown(data.map(member => {
             return ({name: member.name, label: member.name});
           }))
-          setIsLoading(false);
       })
       .catch(err => {
-          setIsLoading(false);
+          setTeamMembers([]);
+          setTeamMembersDropdown([{name: currentUserInfo.name, label: currentUserInfo.name}]);
           console.error(err);
       });
-  },[])
-  
+  }, [])
+
+  useEffect(() => {
+    getAllWigsByTeamId(currentUserInfo.teamId)
+      .then(data => {
+          setAllWigs(data);
+          setCurrentWig(data[0]);
+          setCurrentLeadMeasure(data[0].leadMeasures[0]);
+          setStartDate(new Date(data[0].startDate));
+          setEndDate(new Date(data[0].endDate));
+          setDateRange(handleDateFilter(data[0].leadMeasures[0], new Date(data[0].startDate), new Date(data[0].endDate)));
+      })
+      .catch(err => {
+          setAllWigs([]);
+          setCurrentWig([]);
+          console.error(err);
+      });
+  }, [])
+
   const handleSelectedWig = (e) => {
-    const chartData = wigs.filter(wig => wig.wigId === e.target.value)
-    setChartData(chartData);
-    const lead = chartData[0].leadMeasures[0];
-    setLeadMeasure(lead);
-    const startDate = new Date(chartData[0].startDate);
-    const endDate = new Date(chartData[0].endDate);
-    setStartDate(startDate);
-    setEndDate(endDate);
-    handleDateFilter(startDate, endDate);
+    const wig = allWigs.filter(wig => wig.wigId === e.target.value)
+    setCurrentWig(wig[0]);
+    setCurrentLeadMeasure(wig[0].leadMeasures[0]);
+    setStartDate(new Date(wig[0].startDate));
+    setEndDate(new Date(wig[0].endDate));
+    setDateRange(handleDateFilter(wig[0].leadMeasures[0], new Date(wig[0].startDate), new Date(wig[0].endDate)));
   }
 
-  const handleSelectedMembers = (selectedOption) => {
-    setSeletedTeamMembers(selectedOption);
-  };
-
   const handleSelectedLeadMeasure = (e) => {
-    const currentWig = chartData;
-    const lead = currentWig[0].leadMeasures.filter(lead => lead.leadId === e.target.value);
-    setLeadMeasure(lead[0]);
+    const lead = currentWig.leadMeasures.filter(lead => lead.leadId === e.target.value);
+    setCurrentLeadMeasure(lead[0]);
     setDefaultLeadMeasureDropdownValue(e.target.value);
-    handleDateFilter(new Date(currentWig[0].startDate), new Date(currentWig[0].endDate));
+    setDateRange(handleDateFilter(lead[0], new Date(currentWig.startDate), new Date(currentWig.endDate)));
   }
 
   const onChangeDate = dates => {
     const [start, end] = dates;
     setStartDate(start);
     setEndDate(end);
-    handleDateFilter(start, end);
-    console.log(start)
-    console.log(end);
+    setDateRange(handleDateFilter(currentLeadMeasure, start, end));
   };
 
-  const handleDateFilter = (startDate = chartData[0].startDate, endDate = chartData[0].endDate) => {
-    //const dates = leadMeasure.leadData.map(date => new Date(date.startDate).getTime());
-    const dates = leadMeasure !== undefined 
-      ? leadMeasure.leadData.map(date => new Date(date.startDate).getTime())
-      : [new Date().getTime()];
-    console.log('dates', dates)
-    const start = startDate?.getTime();
-    const end = endDate?.getTime();
-    const range = dates.filter(date => date >= start && date <= end)
-    const formattedDateArray = range.map(date => new Date(date).toISOString().split("T")[0]);
-    setDateRange(formattedDateArray);
+  const handleDateFilter = (currentLead, from, to) => {
+    const startDate = from?.getTime();
+    const endDate = to?.getTime();
+    const leadDates = currentLead.leadData.map(date => new Date(date.startDate).getTime());
+    const range = leadDates.filter(date => date >= startDate && date <= endDate);
+    return range.map(date => new Date(date).toISOString().split("T")[0]);
   }
+
+  const handleSelectedMembers = (selectedOption) => {
+    setSeletedTeamMembers(selectedOption);
+  };
+
 
   const showData = () => {
-    console.log(chartData);
-    console.log(leadMeasure);
+    console.log(currentWig);
+    console.log(currentLeadMeasure);
     console.log(dateRange);
+    console.log(selectedTeamMembers);
   }
-
-
-  
   
   const recentLead = dataSet1.recentLead;
   const dates = dataSet1.dates;
@@ -214,75 +186,76 @@ export default function Scoreboard(){
 
   return (
     <>
-      {isLoading ? <FormLoaderOverlay /> : 
+    {allWigs.length === 0 ? <p>no Wig found</p> : 
       <div>
-      <div>
-          <h1>{wigs.length > 0 ? chartData[0]?.wigName : ''}</h1>
-      </div>
-      <div>
-          <h2>from: {wigs.length > 0 ? chartData[0]?.startDate : ''} until: {wigs.length > 0 ? chartData[0]?.endDate : ''}</h2>
-      </div>
-      <div className="filterOptions"> 
-          <button type="button" onClick={showData}>console.log</button>
-          <section className="filterOption-wig">
-              <label htmlFor="wigSelect" className="form-label">WIG</label>
-              <select onChange={handleSelectedWig} className="form-select" id="wigSelect" name="wigSelect">
-              {wigs.length > 0 && wigs.map((wig, index) => 
-                  <option key={index} value={wig.wigId}>{wig.wigName}</option>
-              )}
-              </select>
-          </section>
-          <section className="filterOption-leadMeasure">
-              <label htmlFor="leadMeasureSelect" className="form-label">Lead Measures</label>
-              <select value={defaultLeadMeasureDropdownValue} onChange={handleSelectedLeadMeasure} className="form-select" id="leadMeasureSelect" name="leadMeasureSelect">
-              {chartData.length > 0 && chartData[0]?.leadMeasures.map((lead, index) => 
-                  <option key={index} value={lead.leadId}>{lead.leadName}</option>
-              )}
-              </select>
-          </section>
-          <section className="filterOption-date">
-            <label className="form-label" htmlFor="datePicker">Date:</label>
-            <DatePicker
-              locale="en"
-              id="datePicker" 
-              name="datePicker" 
-              selected={startDate}
-              onChange={onChangeDate}
-              startDate={startDate}
-              endDate={endDate}
-              selectsRange
-              inline
+        <button type="button" onClick={showData}>console.log</button>
+        <div>
+          <h1>{currentWig?.wigName}</h1>
+        </div>
+        <div>
+          <h2>from: {currentWig?.startDate} until: {currentWig?.endDate}</h2>
+        </div>
+      <div className="filterOptions">
+        <section className="filterOption-wig">
+            <label htmlFor="wigSelect" className="form-label">WIG</label>
+            <select onChange={handleSelectedWig} className="form-select" id="wigSelect" name="wigSelect">
+            {allWigs.map((wig, index) => 
+                <option key={index} value={wig.wigId}>{wig.wigName}</option>
+            )}
+            </select>
+        </section>
+        <section className="filterOption-leadMeasure">
+            <label htmlFor="leadMeasureSelect" className="form-label">Lead Measures</label>
+            <select value={defaultLeadMeasureDropdownValue} onChange={handleSelectedLeadMeasure} className="form-select" id="leadMeasureSelect" name="leadMeasureSelect">
+            {currentWig?.leadMeasures.map((lead, index) => 
+                <option key={index} value={lead.leadId}>{lead.leadName}</option>
+            )}
+            </select>
+        </section>
+        <section className="filterOption-date">
+          <label className="form-label" htmlFor="datePicker">Date:</label>
+          <DatePicker
+            locale="en"
+            id="datePicker" 
+            name="datePicker" 
+            selected={startDate}
+            onChange={onChangeDate}
+            startDate={startDate}
+            endDate={endDate}
+            selectsRange
+            inline
+          />
+        </section>
+        <section className="filterOption-teamMembers">
+            <label htmlFor="teamMember" className="form-label">TeamMember</label>
+            <Select
+              isMulti
+              id="teamMember"
+              name="teamMembers"
+              options={teamMembersDropdown}
+              className="basic-multi-select"
+              classNamePrefix="select"
+              onChange={handleSelectedMembers}
             />
-          </section>
-          <section className="filterOption-teamMembers"></section>
-              <label htmlFor="teamMember" className="form-label">TeamMember</label>
-              <Select
-                isMulti
-                id="teamMember"
-                name="teamMembers"
-                options={teamMembersDropdown}
-                className="basic-multi-select"
-                classNamePrefix="select"
-                onChange={handleSelectedMembers}
-              />
+        </section>
       </div>
-        <div className= 'scoreBoard'>
-            <section className="page-content add-lead-measure-page" style={{width: 400, height: 300}}>
-                <PageHeader pageTitle="Individual lead measures"/> 
-                    <Bar data={dataBar} options={optionsBar} style={{
-                        backgroundColor: 'white',
-                        }}/>
-            </section>
-        </div>
-        <div className= 'scoreBoard'>
-            <section className="page-content add-lead-measure-page" style={{width: 400, height: 300}}>
-                <PageHeader pageTitle="Team lead measures"/> 
-                    <Line data={dataLine} options={optionsLine} style={{
-                        backgroundColor: 'white',
-                        }}/>
-            </section>
-        </div>
-      </div>}
+      <div className= 'scoreBoard'>
+        <section className="page-content add-lead-measure-page" style={{width: 400, height: 300}}>
+            <PageHeader pageTitle="Individual lead measures"/> 
+                <Bar data={dataBar} options={optionsBar} style={{
+                    backgroundColor: 'white',
+                    }}/>
+        </section>
+      </div>
+      <div className= 'scoreBoard'>
+          <section className="page-content add-lead-measure-page" style={{width: 400, height: 300}}>
+              <PageHeader pageTitle="Team lead measures"/> 
+                  <Line data={dataLine} options={optionsLine} style={{
+                      backgroundColor: 'white',
+                      }}/>
+          </section>
+      </div>
+    </div>}
   </>
   )
 }
