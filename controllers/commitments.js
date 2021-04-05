9b8916be3990bdf14eadb2e67a1f725119964be5
lukas-schmid <schmid.luk@hotmail.com>
2021-04-05 19:47:51 +0200

    update and delete commitments

--- controllers/commitments.js
+++ controllers/commitments.js
@@ -106,46 +106,48 @@ exports.createCommitment = async (req, res, next) => {
   }
 }
 
-// delete!
-// exports.updateCommitment = async (req, res, next) => {
-//   const commitmentId = req.params.commitmentId;
-//   const userId = req.body.userId;
-//   const commitmentName = req.body.commitmentName;
-//   const startDate = req.body.startDate;
-//   const wigId = req.body.wigId;
-//   const wigName = req.body.wigName;
-//   const leadId = req.body.leadId;
-//   const category = req.body.category;
-//   const isCompleted = req.body.isCompleted;
-//   try {
-//     await service.updateCommitment(
-//       commitmentId,
-//       userId,
-//       commitmentName,
-//       startDate,
-//       wigId,
-//       wigName,
-//       leadId,
-//       category,
-//       isCompleted
-//     );
-//     const response = await service.getCommitment(commitmentId);
-//     res.status(201).json(response);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+
+exports.updateCommitment = async (req, res, next) => {
+  const commitmentId = req.params.commitmentId;
+  const userId = req.params.userId;
+  const body = [{
+    commitmentId,
+    category: req.body.category,
+    startDate: req.body.startDate,
+    isCompleted: req.body.isCompleted,
+    commitmentName: req.body.commitmentName
+  }];
+  try {
+    const user = await userService.getUser(userId);
+    const commitments = user.commitments;
+    const filteredCommitments = commitments.filter(commitment => commitment.commitmentId !== commitmentId);
+    const newCommitments = filteredCommitments.concat(body);
+    await userService.updateCommitment(
+      userId,
+      newCommitments
+    );
+    const response = await userService.getUser(userId);
+    res.status(200).json(response);
+  } catch (error) {
+    next(error);
+  }
+};
 
 
-// delete!
-// exports.deleteCommitment = async (req, res, next) => {
-//   const commitmentId = req.params.commitmentId;
-//   try {
-//     await service.deleteCommitment(commitmentId);
-//     res
-//       .status(204)
-//       .json({ message: `Commitment with id ${commitmentId} has been deleted` });
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+exports.deleteCommitment = async (req, res, next) => {
+  const commitmentId = req.params.commitmentId;
+  const userId = req.params.userId;
+  try {
+    const user = await userService.getUser(userId);
+    const commitments = user.commitments;
+    const newCommitments = commitments.filter(commitment => commitment.commitmentId !== commitmentId);
+    await userService.deleteCommitment(
+      userId,
+      newCommitments
+    );
+    const response = await userService.getUser(userId);
+    res.status(200).json(response);
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/commitments.js
+++ routes/commitments.js
@@ -4,8 +4,8 @@ const commitments = require("../controllers/commitments");
 
 //router.get("/api/commitments/wigs/:wigId", commitments.getAllCommitmentsByWigId);
 //router.get("/api/commitments/:userId", commitments.getAllCommitments);
+router.put("/api/commitments/:commitmentId/users/:userId", commitments.updateCommitment);
+router.delete("/api/commitments/:commitmentId/users/:userId", commitments.deleteCommitment);
 router.post("/api/commitments/:userId", commitments.createCommitment);
-//router.put("/api/commitments/:commitmentId", commitments.updateCommitment);
-//router.delete("/api/commitments/:commitmentId", commitments.deleteCommitment);
 
 module.exports = router;
