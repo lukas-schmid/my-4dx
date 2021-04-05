b8c3ccbb531a7923216132755d7e4be6cc3bcc04
lukas-schmid <schmid.luk@hotmail.com>
2021-04-05 20:36:30 +0200

    cleaned up commitments

--- routes/commitments.js
+++ routes/commitments.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const commitments = require("../controllers/commitments");
 
-//router.get("/api/commitments/wigs/:wigId", commitments.getAllCommitmentsByWigId);
-//router.get("/api/commitments/:userId", commitments.getAllCommitments);
 router.put("/api/commitments/:commitmentId/users/:userId", commitments.updateCommitment);
 router.delete("/api/commitments/:commitmentId/users/:userId", commitments.deleteCommitment);
 router.post("/api/commitments/:userId", commitments.createCommitment);
