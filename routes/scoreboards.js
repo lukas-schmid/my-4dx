fa7790dfbda4afb74aa2f03e224395c554c4cfc9
lukas-schmid <schmid.luk@hotmail.com>
2021-04-05 20:38:55 +0200

    deleted scoreboard backend files

--- routes/scoreboards.js
+++
@@ -1,9 +0,0 @@
-// delete!
-// const express = require("express");
-// const router = express.Router();
-// const scoreboards = require("../controllers/scoreboards");
-
-// router.post("/api/scoreboards", scoreboards.createScoreboard);
-// router.delete("/api/scoreboards/:scoreboardId", scoreboards.deleteScoreboard);
-
-// module.exports = router;
